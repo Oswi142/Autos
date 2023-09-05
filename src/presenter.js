a6a9346241e3fd1e91bbb8f06fc6346a10ccece8
oswaldoroman <edwin.roman@ucb.edu.bo>
2023-09-05 00:17:30 -0400

    Editamos el presenter y el html

--- src/presenter.js
+++ src/presenter.js
@@ -1,15 +1,13 @@
-import sumar from "./sumador";
+import Autito from "./autito";
 
-const first = document.querySelector("#primer-numero");
-const second = document.querySelector("#segundo-numero");
-const form = document.querySelector("#sumar-form");
-const div = document.querySelector("#resultado-div");
+const form=document.querySelector("#autito-form");
+const comandos=document.querySelector("#comandos-input")
+const div=document.querySelector("#pos-final");
+let autito=new Autito(0,0,"N");
 
-form.addEventListener("submit", (event) => {
+form.addEventListener("submit", (event) => 
+{
   event.preventDefault();
-
-  const firstNumber = Number.parseInt(first.value);
-  const secondNumber = Number.parseInt(second.value);
-
-  div.innerHTML = "<p>" + sumar(firstNumber, secondNumber) + "</p>";
+  const command = comandos.value;
+  div.innerHTML = "<p>" + autito.ejecutar(command) + "</p>";
 });
